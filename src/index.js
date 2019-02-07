39ddefe55512167adf0896f92edcd6076d8bef6f
Steven Fisher <steven.fisher22@gmail.com>
2019-02-07 13:16:25 -0600

    commit 2

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello World</h1>;
+console.log(element);
+
+// @ 18:42 in the video
+// https://www.youtube.com/watch?v=Ke90Tje7VS0&t=1s
